3ffcd071d24c3f18a4ae1cd845c48133e4f2de0c
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-04-05 14:07:10 +0800

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,32 +1,32 @@
-require("dotenv").config();
-const Hapi = require("@hapi/hapi");
-const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NoteService");
-const NotesValidator = require("./validator/notes");
+require('dotenv').config()
+const Hapi = require('@hapi/hapi')
+const notes = require('./api/notes')
+const NotesService = require('./services/postgres/NotesService')
+const NotesValidator = require('./validator/notes')
 
 const init = async () => {
-  const notesService = new NotesService();
+    const notesService = new NotesService()
 
-  const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
-    routes: {
-      cors: {
-        origin: ["*"],
-      },
-    },
-  });
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    })
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
-    },
-  });
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    })
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
-};
+    await server.start()
+    console.log(`Server berjalan pada ${server.info.uri}`)
+}
 
 init();
