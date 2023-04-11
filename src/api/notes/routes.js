bfd8a6ee6d286051736edbe2d64ae50d000a0409
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-04-11 10:49:13 +0800

    authentication implemented

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
     method: 'POST',
     path: '/notes',
     handler: (request, h) => handler.postNoteHandler(request, h),
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes',
     handler: () => handler.getNotesHandler(),
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/notes/{id}',
     handler: (request, h) => handler.getNoteByIdHandler(request, h),
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/notes/{id}',
     handler: (request) => handler.putNoteByIdHandler(request),
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/notes/{id}',
     handler: (request) => handler.deleteNoteByIdHandler(request),
+    options: {
+      auth: 'notesapp_jwt',
+    },
   },
 ];
 
